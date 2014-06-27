d50727b9b85d78759d6dc3a573bbb3c81c97e95b
charlieschwabacher <charlieschwabacher@gmail.com>
2014-06-27 00:33:55 -0500

    work

---
+++ lib/pql.rb
@@ -0,0 +1,5 @@
+module PQL
+end
+
+require 'pql/node_extensions'
+require 'pql/parser'
---
+++ lib/pql/parser.rb
@@ -0,0 +1,30 @@
+class PQL::Parser
+
+  Treetop.load(File.join(File.expand_path(File.dirname(__FILE__)), 'grammar.treetop'))
+  @@parser = PQLParser.new
+
+
+  def self.parse(expression)
+    tree = @@parser.parse expression
+
+    if tree.nil?
+      raise Exception, "#{@@parser.failure_reason} at #{@@parser.failure_line}:#{@@parser.failure_column}"
+    end
+
+    tree
+  end
+
+
+  def self.print(root_node, offset = 0)
+    unless root_node.syntax_node?
+      text = root_node.text_value.gsub("\n",'').split(' ').join(' ').strip
+      text = text[0, 100] + '...' if text.length > 100
+      puts "#{'  ' * offset} #{root_node.class.name} - '#{text}' : #{root_node.interval}"
+
+      root_node.elements.each {|node| self.print node, offset + 1 } if root_node.elements
+    else
+      root_node.elements.each {|node| self.print node, offset } if root_node.elements
+    end
+  end
+
+end
